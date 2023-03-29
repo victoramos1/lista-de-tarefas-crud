d25dfb814b1ad0cec4bf1afdce0178b01b6dc0b2
Victor Ramos <ramos_victor@outlook.com>
2023-03-28 22:20:17 -0300

    Criação de inserção e exclusão de dados

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import styles from './Main.module.css'
+import { useState, useRef } from 'react'
+
+export default function Main(){
+    
+        let dadoCapturado = useRef('')
+        const [dadosGuardados, setDadosGuardados] = useState([])
+
+        function inserirNovoDado(){ 
+
+            setDadosGuardados(dadosAnteriores => [...dadosAnteriores, dadoCapturado.current.value])
+        }
+
+        function excluirDado(index){
+            let novosDados = [...dadosGuardados]
+            novosDados.splice(index, 1)
+            setDadosGuardados(novosDados)
+        }
+
+    return(
+        <>
+            <div className={styles.containerNativo}>
+                <input id="input-dados" name="input-dados" ref={dadoCapturado}></input>
+                <button onClick={inserirNovoDado}>Adicionar</button>
+                {dadosGuardados.map((item, index) =>
+                    <div className={styles.containerElemento} key={index}>
+                        <p>{item}</p>
+                        <button className={styles.btn}>Editar</button>
+                        <button className={styles.btn} onClick={(item) => excluirDado(index)}>Excluir</button>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
