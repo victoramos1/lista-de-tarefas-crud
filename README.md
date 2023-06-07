ddde00809781e3e6e27e2df8fe020e93e92a392c
victoramos1 <ramos_victor@outlook.com>
2023-06-06 22:31:55 -0300

    Criação de modal, revisão se responsividade e edição do ReadMe

--- README.md
+++ README.md
@@ -22,19 +22,20 @@
 <h2>Inicialização</h2>
 
 <p>1 - Instale o Node.Js para conseguir rodar o React. Caso possua instalado, pule essa etapa</p>
-<p>2 - No terminal da IDE, use o comando "git clone https://github.com/victoramos1/lista-de-tarefas-crud" (sem aspas) ou faça o download do arquivo .zip diretamente do Github.</p>
-<p>3 - Ainda com a pasta aberta na IDE, rode o comando "npm install" (sem aspas) no terminal da IDE. É necessário que o caminho mostrado no terminal da IDE seja do local do projeto.</p>
-<p>4 - No termina da IDE, rode o comando "npm start" (sem aspas) e aguarde uma aba no seu navegador abrir com o projeto em execução.</p>
 
+<p>2 - No terminal do VS Code (ou na IDE que você estiver utilizando), utilize o comando "git clone https://github.com/victoramos1/lista-de-tarefas-crud" (sem aspas) caso tenha o Git instalado no computador, ou faça o download do arquivo .zip diretamente do GitHub.</p>
+
+<p>3 - Com a pasta aberta na IDE (o caminho mostrado no terminal precisa levar ao projeto no seu computador, como mostrado na imagem abaixo), execute o comando "npm install" (sem aspas) no terminal da IDE.</p>
+<img src="./src/img/caminho.png">
+
+<p>4 - No termina da IDE, rode o comando "npm start" (sem aspas) e aguarde uma aba no seu navegador abrir com o projeto em execução.</p>
 
 <h2>Implementações futuras</h2>
 
 <ul>
-    <li>Possibilidade de ticar anotações concluídas</li>
-    <li>Revisão da responsividade considerando a altura do dispositivo do usuário</li>
     <li>Uma nova tela de edição de anotações</li>
 </ul>
 
 <h2>Status do projeto</h2>
 
-<p>Versão 1.0 finalizada. Atualizações citadas em "implementações futuras" em breve.</p>
+<p>Versão 1.1.0 finalizada.</p>
---
+++ src/pages/Main.js
@@ -0,0 +1,82 @@
+import './Main.css'
+import { useState, useRef } from 'react'
+import editar from '../img/editar.svg'
+import deletar from '../img/deletar.svg'
+
+export default function Main() {
+
+  let dadoCapturado = useRef('')
+  let dadoEditado = useRef('')
+  const [dadosGuardados, setDadosGuardados] = useState([])
+  const [modalAtivado, setModalAtivado] = useState(false)
+  const [indiceParaEdicao, setIndiceParaEdicao] = useState("")
+
+  function insereNovoDado() {
+    if (dadoCapturado.current.value === '') {
+      window.alert('Por favor, digite algo válido')
+    } else {
+      setDadosGuardados(dadosAnteriores => [...dadosAnteriores, dadoCapturado.current.value])
+    }
+  }
+
+  function excluiDado(index) {
+    let novosDados = [...dadosGuardados]
+    novosDados.splice(index, 1)
+    setDadosGuardados(novosDados)
+  }
+
+  function ativaModal(index) {
+    setModalAtivado(true)
+    passaNumeroDoIndice(index)
+  }
+
+  function passaNumeroDoIndice(index) {
+    setIndiceParaEdicao(index)
+  }
+
+  function editaDados() {
+    let novosDados = [...dadosGuardados]
+    novosDados[indiceParaEdicao] = dadoEditado.current.value
+    if (novosDados[indiceParaEdicao] === '') {
+      window.alert('Por favor, digite algo válido')
+    } else {
+      setDadosGuardados(novosDados)
+      setModalAtivado(false)
+    }
+  }
+
+  return (
+    <>
+      {/* Modal */}
+      <div className={modalAtivado === true ? "modal" : "oculto"}>
+        <a href="#!" onClick={() => setModalAtivado(false)}>X</a>
+        <div className="div">
+          <input placeholder="Digite a alteração que deseja" ref={dadoEditado}></input>
+          <button onClick={() => editaDados()}>Concluir</button>
+        </div>
+      </div>
+      {/* Homepage */}
+      <div className="containerNativo">
+        <div className="containerElemento">
+          <div className="titulo">
+            <h1>Anotações</h1>
+          </div>
+          <div className="preenchimento">
+            <input id="input-dados" name="input-dados" placeholder="Digite aqui" ref={dadoCapturado}></input>
+            <button onClick={insereNovoDado}>Adicionar</button>
+          </div>
+          {dadosGuardados.map((item, index) =>
+            <div className="itensArray" key={index}>
+              <p>{item}</p>
+              <div>
+                <a href="#!" onClick={() => ativaModal(index)}><img src={editar} alt="Desenho de um desenho e uma folha, em alusão ao ato de editar algo" /></a>
+                <a href="#!" onClick={() => excluiDado(index)}><img src={deletar} alt="Desenho de uma lixeira, em alusão ao ato de excluir algo" /></a>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
